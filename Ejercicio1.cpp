97215dc78dfee8b359e3651bc87b8a45e0dd0533
Gabriel De Ornelas <142519693+gabodornelas@users.noreply.github.com>
2024-05-03 13:48:45 -0400

    Add files via upload

---
+++ Ejercicio1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int x;
+  while(true){
+    cin >> x;
+    if (x != 42){
+      cout << x << endl;
+    }else{
+      break;
+    }
+  }
+    return 0;
+}
